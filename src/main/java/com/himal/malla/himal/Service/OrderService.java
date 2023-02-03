89cf02e1d3e579266dc13e5321186ad345664afe
Himalaya04 <116843111+Himalaya04@users.noreply.github.com>
2023-02-03 11:46:07 -0500

    i change order service create order

--- src/main/java/com/himal/malla/himal/Service/OrderService.java
+++ src/main/java/com/himal/malla/himal/Service/OrderService.java
@@ -41,4 +41,46 @@ public class OrderService {
         orderRepo.deleteById(orderId);
         return "Order has been removed " + orderId ;
     }
+    
+        public Order createOrder(Order order) {
+        //pseudoCode
+        //if that user is exist
+        //if it does exist ->>> check if product exist
+        //yes ->>true
+        // We need to calculate the sales tax, total amount,
+        //response "order successfully created "
+        //no ->>false
+        //return "give message"
+        //if it doesn't exist ->>>
+        User existingUser = userRepo.findById(order.getUser().getUserId()).get();
+        if (existingUser != null) {
+            Product existingProduct = productRepo.findById(order.getProduct().getProductId()).get();
+
+
+            if (existingProduct != null) {
+
+
+                // existingProduct.getPrice();
+                // order.getQuantity();
+
+                int amount = existingProduct.getPrice() * order.getQuantity();
+                order.setPrice(amount);
+
+                //user info , product info, quanty, totalmount (price) save to db
+                //call orderrepo save
+
+                //give 5% discount if you've ordered more than $100 of amount
+
+
+            } else if (existingProduct.getPrice()>=100) {
+                int discountAmt = (existingProduct.getPrice() * order.getQuantity()) *(5/100);
+                int afterDiscount = (existingProduct.getPrice() * order.getQuantity()) -discountAmt;
+                order.setPrice(afterDiscount);
+
+            }
+
+        }
+
+        return orderRepo.save(order);
+    }
 }
